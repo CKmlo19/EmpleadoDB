6f44a0597bd1057e68c9648108499ae0641d649d
Camilo Orocú <luiscamilooc@gmail.com>
2024-03-07 21:46:13 -0600

    Los datos para insertar y listar de la db

--- EmpleadoDB/Data/EmpleadoDatos.cs
+++ EmpleadoDB/Data/EmpleadoDatos.cs
@@ -37,5 +37,35 @@ namespace EmpleadoDB.Data
             }
             return oLista;
         }
+
+        public bool Guardar(EmpleadoModel oEmpleado) {
+            bool resultado;
+
+            try{ 
+
+
+                var cn = new Conexion();
+
+            // abre la conexion
+            using (var conexion = new SqlConnection(cn.getCadenaSQL()))
+            {
+                conexion.Open();
+                // el procedure de listar
+                SqlCommand cmd = new SqlCommand("dbo.InsertarEmpleado", conexion);
+                cmd.Parameters.AddWithValue("inNombre",oEmpleado.Nombre);
+                cmd.Parameters.AddWithValue("inSalario",oEmpleado.Salario);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.ExecuteNonQuery(); // sirve para que ejecute el store procedure como tal, que es la que inserta
+   
+            }
+                resultado = true;
+            
+            }catch(Exception e) {
+                string error = e.Message;
+                resultado = false; 
+                    
+            }
+            return resultado;
+        }
     }
 }
