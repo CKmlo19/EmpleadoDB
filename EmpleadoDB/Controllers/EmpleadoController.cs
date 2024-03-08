58d2420de41fbb13ad8598901efa1181985dd935
Camilo Orocú <luiscamilooc@gmail.com>
2024-03-08 13:44:29 -0600

    Creando unas validaciones con para el formulario con el Salario

--- EmpleadoDB/Controllers/EmpleadoController.cs
+++ EmpleadoDB/Controllers/EmpleadoController.cs
@@ -25,6 +25,12 @@ namespace EmpleadoDB.Controllers
         {
             // este otro es para capturar los datos y enviarlo a la base de datos
 
+            //validacion de los campos
+            if (!ModelState.IsValid)
+            { // funcion propia que sirve para saber si un campo esta vacio, true si todo bien, false si hay algo malo
+                return View();
+            }
+
             var resultado = _EmpleadoDatos.Insertar(oEmpleado);
 
             if (resultado)
--- EmpleadoDB/Models/EmpleadoModel.cs
+++ EmpleadoDB/Models/EmpleadoModel.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
 using Microsoft.AspNetCore.Routing.Constraints;
 using System.Data.SqlTypes;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Diagnostics.CodeAnalysis;
 
 namespace EmpleadoDB.Models
 {
     public class EmpleadoModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage="El campo Nombre es obligatorio")]
         public string? Nombre { get; set; }
+        [Required(ErrorMessage = "El campo Salario es obligatorio")]
         public decimal Salario { get; set; }
     }
 }
